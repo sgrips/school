f3538d09a45e72afd111f6d90f700c72699e9f83
Antonio Sgrignoli <asgrignoli@live.com>
2023-03-03 14:19:45 +0100

    aggiunta stampa stacktrace

--- src/main/java/it/brt/helloworld/listeners/KafkaParentTopicListenerService.java
+++ src/main/java/it/brt/helloworld/listeners/KafkaParentTopicListenerService.java
@@ -1,6 +1,8 @@
 package it.brt.helloworld.listeners;
 
 import it.brt.helloworld.models.StudentJustification;
+import it.brt.helloworld.repositories.ClasseRepository;
+import it.brt.helloworld.services.ClasseService;
 import it.brt.helloworld.services.StudentJustificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 import it.brt.school.course.parentstudents.models.StudentJustificationMessage;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class KafkaParentTopicListenerService
@@ -21,19 +24,34 @@ public class KafkaParentTopicListenerService
     @Autowired
     StudentJustificationService studentJustificationService;
 
-   @KafkaListener(
+    @Autowired
+    ClasseService classeService;
+
+
+    @KafkaListener(
             topics =  "#{'${spring.kafka.topics}'.split(',')}",
             groupId = "groupid",
             containerFactory = "studentJustificationKafkaListenerContainerFactory")
     public void studentJustificationListener(StudentJustificationMessage studentJustificationMessage) {
-       studentJustificationService.createStudentJustification(new StudentJustification() {{
-           setAbsentFrom(studentJustificationMessage.getAbsentFrom());
-           setAbsentTo(studentJustificationMessage.getAbsentTo());
-           setParentSignature(studentJustificationMessage.getParentSignature());
-           setStudentName(studentJustificationMessage.getStudentName());
-           setStudentLastName(studentJustificationMessage.getStudentLastName());
-           setCreateTime(new Date());
-       }});
+
+        try{
+            studentJustificationService.createStudentJustification(new StudentJustification()
+                    {{
+                    setAbsentFrom(studentJustificationMessage.getAbsentFrom());
+            setAbsentTo(studentJustificationMessage.getAbsentTo());
+            setParentSignature(studentJustificationMessage.getParentSignature());
+            setStudentName(studentJustificationMessage.getStudentName());
+            setStudentLastName(studentJustificationMessage.getStudentLastName());
+            setCreateTime(new Date());
+        }})
+            ;
+
+        }   catch (Exception e) {
+            System.err.println(e );
+            e.printStackTrace();
+        }
+
+
     }
 
 }
